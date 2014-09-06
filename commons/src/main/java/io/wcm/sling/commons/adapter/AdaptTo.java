c931a3302b5a8e9629039f95230a0c349efd46b9
sseifert <sseifert@pro-vision.de>
2014-09-06 15:53:09 +0200

    add AdaptTo util class

---
+++ commons/src/main/java/io/wcm/sling/commons/adapter/AdaptTo.java
@@ -0,0 +1,48 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.sling.commons.adapter;
+
+import org.apache.sling.api.adapter.Adaptable;
+
+/**
+ * Utility methods around Sling {@link Adaptable}.
+ */
+public final class AdaptTo {
+
+  private AdaptTo() {
+    // static methods only
+  }
+
+  /**
+   * Try to adapt the adaptable to the given type and ensures that it succeeds.
+   * @param adaptable Adaptable
+   * @param type Type
+   * @return Adaption result (not null)
+   * @throws UnableToAdaptException if the adaption was not successful
+   */
+  public static <T> T notNull(Adaptable adaptable, Class<T> type) {
+    T object = adaptable.adaptTo(type);
+    if (object == null) {
+      throw new UnableToAdaptException(adaptable, type);
+    }
+    return object;
+  }
+
+}
---
+++ commons/src/main/java/io/wcm/sling/commons/adapter/UnableToAdaptException.java
@@ -0,0 +1,58 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.sling.commons.adapter;
+
+import org.apache.sling.api.adapter.Adaptable;
+
+/**
+ * Exception is thrown if {@link AdaptTo#notNull} call was not successful.
+ */
+public class UnableToAdaptException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+
+  private final Adaptable adaptable;
+  private final Class<?> type;
+
+  /**
+   * @param adaptable Adaptable object instance
+   * @param type Interface to adapt to
+   */
+  public UnableToAdaptException(Adaptable adaptable, Class<?> type) {
+    super("Unable to adapt " + adaptable + " to " + type.getName());
+    this.adaptable = adaptable;
+    this.type = type;
+  }
+
+  /**
+   * @return Adaptable object instance
+   */
+  public Adaptable getAdaptable() {
+    return adaptable;
+  }
+
+  /**
+   * @return Interface to adapt to
+   */
+  public Class<?> getType() {
+    return type;
+  }
+
+}
