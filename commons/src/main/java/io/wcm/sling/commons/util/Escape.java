f12bf8aa4ffecd022312a913f5107fd733e3166e
sseifert <sseifert@pro-vision.de>
2014-09-16 17:30:00 +0200

    add Escape

---
+++ commons/src/main/java/io/wcm/sling/commons/util/Escape.java
@@ -0,0 +1,98 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.sling.commons.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.commons.lang3.CharEncoding;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Encoding utility functions.
+ */
+public final class Escape {
+
+  private Escape() {
+    // Utility class - no instancing allowed
+  }
+
+  /**
+   * Applies URL-Encoding to the given parameter name or value. Uses {@link URLEncoder#encode(String, String)} with
+   * UTF-8 character set, while avoiding the need to catch the UnsupportedEncodingException.
+   * @param value the parameter name or value to encode
+   * @return URL-encoded string - or empty string if the specified value was null
+   * @throws RuntimeException in the very unlikely case that UTF-8 is not supported on the current system
+   */
+  public static String urlEncode(String value) {
+    if (value == null) {
+      return "";
+    }
+    try {
+      return URLEncoder.encode(value, CharEncoding.UTF_8);
+    }
+    catch (UnsupportedEncodingException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  /**
+   * Creates a valid node name. Replaces all chars not in a-z, A-Z and 0-9 or '_' with '-' and converts all to lowercase.
+   * @param value String to be labelized.
+   * @return The labelized string.
+   */
+  public static String validName(String value) {
+
+    // convert to lowercase
+    String text = value.toLowerCase();
+
+    // replace some special chars first
+    text = StringUtils.replace(text, "ä", "ae");
+    text = StringUtils.replace(text, "ö", "oe");
+    text = StringUtils.replace(text, "ü", "ue");
+    text = StringUtils.replace(text, "ß", "ss");
+
+    // replace all invalid chars
+    StringBuilder sb = new StringBuilder(text);
+    for (int i = 0; i < sb.length(); i++) {
+      char ch = sb.charAt(i);
+      if (!((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || (ch == '_'))) {
+        ch = '-';
+        sb.setCharAt(i, ch);
+      }
+    }
+    return sb.toString();
+  }
+
+  /**
+   * Create valid filename by applying method {@link Escape#validName(String)} on filename and extension.
+   * @param value Filename
+   * @return Valid filename
+   */
+  public static String validFilename(String value) {
+    String fileExtension = StringUtils.substringAfterLast(value, ".");
+    String fileName = StringUtils.substringBeforeLast(value, ".");
+    if (StringUtils.isEmpty(fileExtension)) {
+      return validName(fileName);
+    }
+    return validName(fileName) + "." + validName(fileExtension);
+  }
+
+}
---
+++ commons/src/test/java/io/wcm/sling/commons/util/EscapeTest.java
@@ -0,0 +1,66 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2014 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.sling.commons.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.apache.commons.lang3.CharEncoding;
+import org.junit.Test;
+
+/**
+ * Test {@link Escape} class.
+ */
+public class EscapeTest {
+
+  @Test
+  public void testUrlEncode() throws UnsupportedEncodingException {
+    assertEquals(URLEncoder.encode("abc", CharEncoding.UTF_8), Escape.urlEncode("abc"));
+    assertEquals(URLEncoder.encode("Abc_äöüÄÖÜß", CharEncoding.UTF_8), Escape.urlEncode("Abc_äöüÄÖÜß"));
+    assertEquals(URLEncoder.encode("Der Jodelkaiser", CharEncoding.UTF_8), Escape.urlEncode("Der Jodelkaiser"));
+    assertEquals(URLEncoder.encode("Der  Jodelkaiser", CharEncoding.UTF_8), Escape.urlEncode("Der  Jodelkaiser"));
+    assertEquals(URLEncoder.encode("äölsa§$5x !?_", CharEncoding.UTF_8), Escape.urlEncode("äölsa§$5x !?_"));
+  }
+
+  @Test
+  public void testValidName() {
+    assertEquals("abc", Escape.validName("abc"));
+    assertEquals("abc_aeoeueaeoeuess", Escape.validName("Abc_äöüÄÖÜß"));
+    assertEquals("der-jodelkaiser", Escape.validName("Der Jodelkaiser"));
+    assertEquals("der--jodelkaiser", Escape.validName("Der  Jodelkaiser"));
+    assertEquals("aeoelsa--5x---_", Escape.validName("äölsa§$5x !?_"));
+    assertEquals("test-file", Escape.validName("test.file"));
+    assertEquals("test-sel1-sel2-file", Escape.validName("test.sel1.sel2.file"));
+  }
+
+  @Test
+  public void testValidFilename() {
+    assertEquals("abc", Escape.validFilename("abc"));
+    assertEquals("abc_aeoeueaeoeuess", Escape.validFilename("Abc_äöüÄÖÜß"));
+    assertEquals("der-jodelkaiser", Escape.validFilename("Der Jodelkaiser"));
+    assertEquals("der--jodelkaiser", Escape.validFilename("Der  Jodelkaiser"));
+    assertEquals("aeoelsa--5x---_", Escape.validFilename("äölsa§$5x !?_"));
+    assertEquals("test.file", Escape.validFilename("test.file"));
+    assertEquals("test-sel1-sel2.file", Escape.validFilename("test.sel1.sel2.file"));
+  }
+
+}
