1e2d41f55b55ceb8fa5419e6804545ace5093195
sseifert <sseifert@pro-vision.de>
2015-09-09 11:52:56 +0200

    Update to Sling Models 1.2.0.

--- models/src/main/java/io/wcm/sling/models/annotations/AemObject.java
+++ models/src/main/java/io/wcm/sling/models/annotations/AemObject.java
@@ -29,6 +29,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import org.apache.sling.models.annotations.Source;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.spi.injectorspecific.InjectAnnotation;
 
 import com.adobe.granite.xss.XSSAPI;
@@ -177,10 +178,22 @@ public @interface AemObject {
   String name() default "";
 
   /**
+   * if set to REQUIRED injection is mandatory, if set to OPTIONAL injection is optional, in case of DEFAULT
+   * the standard annotations ({@link org.apache.sling.models.annotations.Optional},
+   * {@link org.apache.sling.models.annotations.Required}) are used.
+   * If even those are not available the default injection strategy defined on the
+   * {@link org.apache.sling.models.annotations.Model} applies.
+   * Default value = DEFAULT.
+   */
+  InjectionStrategy injectionStrategy() default InjectionStrategy.DEFAULT;
+
+  /**
    * If set to true, the model can be instantiated even if there is no request attribute
    * with the given name found.
    * Default = false.
+   * @deprecated Use {@link #injectionStrategy()} instead
    */
+  @Deprecated
   boolean optional() default false;
 
 }
--- models/src/main/java/io/wcm/sling/models/injectors/impl/AemObjectInjector.java
+++ models/src/main/java/io/wcm/sling/models/injectors/impl/AemObjectInjector.java
@@ -35,12 +35,13 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.scripting.SlingBindings;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.spi.AcceptsNullName;
 import org.apache.sling.models.spi.DisposalCallbackRegistry;
 import org.apache.sling.models.spi.Injector;
-import org.apache.sling.models.spi.injectorspecific.AbstractInjectAnnotationProcessor;
-import org.apache.sling.models.spi.injectorspecific.InjectAnnotationProcessor;
-import org.apache.sling.models.spi.injectorspecific.InjectAnnotationProcessorFactory;
+import org.apache.sling.models.spi.injectorspecific.AbstractInjectAnnotationProcessor2;
+import org.apache.sling.models.spi.injectorspecific.InjectAnnotationProcessor2;
+import org.apache.sling.models.spi.injectorspecific.StaticInjectAnnotationProcessorFactory;
 import org.osgi.framework.Constants;
 
 import com.adobe.granite.xss.XSSAPI;
@@ -67,7 +68,7 @@ import com.day.cq.wcm.commons.WCMUtils;
  * pre-configured from the current request.
  */
 @Property(name = Constants.SERVICE_RANKING, intValue = 4500)
-public final class AemObjectInjector implements Injector, InjectAnnotationProcessorFactory, AcceptsNullName {
+public final class AemObjectInjector implements Injector, StaticInjectAnnotationProcessorFactory, AcceptsNullName {
 
   /**
    * Injector name
@@ -295,7 +296,7 @@ public final class AemObjectInjector implements Injector, InjectAnnotationProces
   }
 
   @Override
-  public InjectAnnotationProcessor createAnnotationProcessor(final Object adaptable, final AnnotatedElement element) {
+  public InjectAnnotationProcessor2 createAnnotationProcessor(final AnnotatedElement element) {
     // check if the element has the expected annotation
     AemObject annotation = element.getAnnotation(AemObject.class);
     if (annotation != null) {
@@ -304,7 +305,7 @@ public final class AemObjectInjector implements Injector, InjectAnnotationProces
     return null;
   }
 
-  private static class AemObjectAnnotationProcessor extends AbstractInjectAnnotationProcessor {
+  private static class AemObjectAnnotationProcessor extends AbstractInjectAnnotationProcessor2 {
 
     private final AemObject annotation;
 
@@ -313,8 +314,14 @@ public final class AemObjectInjector implements Injector, InjectAnnotationProces
     }
 
     @Override
+    public InjectionStrategy getInjectionStrategy() {
+      return annotation.injectionStrategy();
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
     public Boolean isOptional() {
-      return this.annotation.optional();
+      return annotation.optional();
     }
   }
 
